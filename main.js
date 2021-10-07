a8b73644395ab7c5bc6941c96a79d8684920c1d3
tooooo1 <wer4272@nate.com>
2021-10-08 01:24:32 +0900

    Add

---
+++ main.js
@@ -0,0 +1,27 @@
+const id = document.querySelector('#id');
+
+const pw1 = document.querySelector('#pswd1');
+const pwMsg = document.querySelector('#alertTxt');
+const pwImg1 = document.querySelector('#pswd1_img1');
+
+const pw2 = document.querySelector('#pswd2');
+const pwImg2 = document.querySelector('#pswd2_img1');
+const pwMsgArea = document.querySelector('#int_pass');
+
+const userName = document.querySelector('#name');
+
+const yy = document.querySelector('#yy');
+const mm = document.querySelector('#mm');
+const dd = document.querySelector('#dd');
+
+const gender = document.querySelector('#gender');
+
+const email = document.querySelector('#email');
+
+const mobile = document.querySelector('#mobile');
+
+const error = document.querySelectorAll('.error_next_box');
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
